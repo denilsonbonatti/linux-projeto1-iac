2e7be37fc89001ce73f0087a1663da73d51aab4a
Thiago-Hassan <thiagovenonsec@gmail.com>
2023-02-04 21:00:36 -0300

    Arquivos IaC v.1 Fiz um copia e cola, para aprender, depois refiz acrscentando o uma identificação1 no final.
    Infraestrutura como Código: Script de Criação de Estrutura de Usuários, Diretórios e Permissões

--- iac1.sh
+++ iac1.sh
@@ -1,11 +1,3 @@
-#!/bin/bash
-
-echo "Criando diretórios..."
-
-mkdir /publico
-mkdir /adm
-mkdir /ven
-mkdir /sec
 
 echo "Criando grupos de usuários..."
 
@@ -39,4 +31,58 @@ chmod 770 /sec
 chmod 777 /publico
 
 echo "Fim....."
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#!/bin/bash
+
+# Criando os diretórios 1
+mkdir /publico1
+mkdir /adm1
+mkdir /ven1
+mkdir /sec1
+
+# Criando os grupos 1
+groupadd GPR_ADM1
+groupadd GPR_VEN1
+groupadd GPR_SEC1
+
+# Criando os usuários para o grupo GPR_ADM 1
+useradd -m -d /home/carlos -g GPR_ADM carlos1
+useradd -m -d /home/maria -g GPR_ADM maria1
+useradd -m -d /home/joao -g GPR_ADM joao1
+
+# Criando os usuários para o grupo GPR_VEN1
+useradd -m -d /home/debora -g GPR_VEN debora1
+useradd -m -d /home/sebastiana -g GPR_VEN sebastiana1
+useradd -m -d /home/roberto -g GPR_VEN roberto1
+
+# Criando os usuários para o grupo GPR_SEC 1
+useradd -m -d /home/josefina -g GPR_SEC josefina1
+useradd -m -d /home/amanda -g GPR_SEC amanda1
+useradd -m -d /home/rogerio -g GPR_SEC rogerio1
+
+# Configurando as permissões dos diretórios 1
+chown -R root:root /publico1
+chmod 775 /publico1
+
+chown -R root:GPR_ADM /adm1
+chmod 770 /adm1
+
+chown -R root:GPR_VEN /ven1
+chmod 770 /ven1
+
+chown -R root:GPR_SEC /sec1
+chmod 770 /sec1
 
